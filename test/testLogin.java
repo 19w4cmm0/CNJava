461dae678f2ae6b8ef32702adfa2c70492e31ad3
19w4cmm0 <125661366+19w4cmm0@users.noreply.github.com>
2023-12-18 12:06:32 +0700

    Add files via upload

---
+++ test/testLogin.java
@@ -0,0 +1,23 @@
+
+import View.LoginForm;
+import javax.swing.SwingUtilities;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class testLogin {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginForm().setVisible(true);
+            }
+        });
+    }
+}
